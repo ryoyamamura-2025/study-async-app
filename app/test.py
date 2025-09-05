5e5ca1ed3abf301007082acc0f09aaa4e08c683b
ryoyamamura <ryo.bananadaiski@gmail.com>
2025-09-05 23:15:15 +0900

    fix: classを作成

--- app/test.py
+++ app/test.py
@@ -8,26 +8,35 @@
 
 # asyncio.run(main())
 
-
+import domain.gemini as gem
 import json
-from pathlib import Path
-import domain.postprocessing as ps
+import asyncio
 
-# 読み込みたい JSON ファイルのパス
-json_path = Path("output/sample.json")
+async def main():
+    res_json, _ = await gem.video_summary_with_caption_v2()
+    print(json.dumps(res_json, indent=2, ensure_ascii=False))
 
-# ファイルを読み込み、Python の dict に変換
-with json_path.open(encoding="utf-8") as f:
-    data = json.load(f)
+asyncio.run(main())
 
-# 読み込んだ内容を確認
-print(type(data))   # dict
-print(data["title"])  # タイトルを確認
+# import json
+# from pathlib import Path
+# import domain.postprocessing as ps
 
-# テンプレート読込
-template_str = ps.PAGE_TEMPLATE_PATH.read_text(encoding="utf-8")
-# HTML 生成
-html_out = ps.render_page_for_first_summary(template_str, data)
+# # 読み込みたい JSON ファイルのパス
+# json_path = Path("output/sample.json")
 
-# 出力（標準出力）
-print(html_out)
+# # ファイルを読み込み、Python の dict に変換
+# with json_path.open(encoding="utf-8") as f:
+#     data = json.load(f)
+
+# # 読み込んだ内容を確認
+# print(type(data))   # dict
+# print(data["title"])  # タイトルを確認
+
+# # テンプレート読込
+# template_str = ps.PAGE_TEMPLATE_PATH.read_text(encoding="utf-8")
+# # HTML 生成
+# html_out = ps.render_page_for_first_summary(template_str, data)
+
+# # 出力（標準出力）
+# print(html_out)
